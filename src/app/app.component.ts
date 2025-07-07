1218dbb92f17620e38e161e038fa0d58ded212e5
CarlaHoffmann <carlahoffmann@gmx.de>
2025-07-07 13:31:58 +0200

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    MatIconModule, 
+    MatToolbarModule, 
+    MatSidenavModule,
+    RouterModule
+    // DashboardComponent,
+    // UserComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
