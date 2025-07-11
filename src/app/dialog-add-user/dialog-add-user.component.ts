bb5545ca90a79540de0a7eefb2209ce304320bdc
CarlaHoffmann <carlahoffmann@gmx.de>
2025-07-11 13:12:54 +0200

    added input and form fields

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [MatDialogModule, FormsModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss',
+  providers: [provideNativeDateAdapter()],
+})
+export class DialogAddUserComponent {
+  onNoClick() {}
+}
